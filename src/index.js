21830894d2501e6a3410fd683419ab52bcb2d672
Sachin C <sachin@springboard.com>
2019-06-19 12:59:15 +0530

    added arrays and filtering for custom data type

--- src/index.js
+++ src/index.js
@@ -3,13 +3,55 @@ import { type } from 'os';
 
 // Scalar types - String, Boolean, Int, Float, ID
 
+//Demo user data 
+
+const users = [{
+    id:'1',
+    name: 'sachin',
+    email: 'sachin@gmail.com',
+    age: 25
+}, 
+{
+    id: '2',
+    name: 'chethan',
+    email: 'chethan@gmail.com',
+    age: 26
+}]
+
+const posts = [{
+    id: '1',
+    title: 'first post',
+    body: 'content for post 1',
+    published: true
+},
+{
+    id: '2',
+    title: 'second post',
+    body: 'content for post 2',
+    published: true
+},
+{
+    id: '3',
+    title: 'third post',
+    body: 'content for post 3',
+    published: true
+},
+{
+    id: '4',
+    title: 'fourth post',
+    body: 'content for post 4',
+    published: true
+}]
+
+
+
 // Type definitions (schema)
 const typeDefs = `
     type Query {
         me: User!
         post: Post!
-        greeting(name: String): String!
-        add(a: Float, b: Float): Float!
+        users(query: String): [User]!
+        posts(query: String): [Post]!
     }
 
     type User {
@@ -44,15 +86,24 @@ const resolvers = {
                 published: true
             }
         },
-        greeting(parent, args, context) {
-            if (args.name) {
-                return `Hello ${args.name}!`
-            } else {
-                return 'Hello!'
+        users(parent, args, ctx, info) {
+            if(!args.query) {
+                return users;
             }
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase());
+            });
         },
-        add(parent, {a, b}, context) {
-            return a + b;
+        posts(parent, {query}, ctx, info) {
+            if(!query) {
+                return posts;
+            }
+
+            return posts.filter((post) => {
+                const isTitleMatch = post.title.toLowerCase().includes(query.toLowerCase())
+                const isBodyeMatch = post.body.toLowerCase().includes(query.toLowerCase())
+                return isTitleMatch || isBodyeMatch
+            })
         }
     }
 }
